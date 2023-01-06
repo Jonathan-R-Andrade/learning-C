bc383c83d20ffc3ed962e7a12603bb075b5e9f0f
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-01-06 04:41:52 -0300

    batalha_naval

--- src/batalha_naval.c
+++ src/batalha_naval.c
@@ -7,28 +7,28 @@ int navio[3][2];
 int tiro[2];
 int tentativas = 0, acertos = 0;
 
-void inicializaTabuleiro();
-void mostraTabuleiro();
-void iniciaNavios();
-void darTiro();
+void inicializar_tabuleiro();
+void mostrar_tabuleiro();
+void iniciar_navios();
+void atirar();
 int acertou(int linha, int coluna);
 void dica();
-void alteraTabuleiro(char pos);
-int verificaPosicao();
-void limparTerminal();
+void alterar_tabuleiro(char pos);
+int verificar_posicao();
+void limpar_terminal();
 
 int main(void) {
     int continuar = 1, c = 1;
 
     do {
-        inicializaTabuleiro();
-        iniciaNavios();
+        inicializar_tabuleiro();
+        iniciar_navios();
         do {
-            mostraTabuleiro();
-            darTiro();
+            mostrar_tabuleiro();
+            atirar();
         } while (acertos < 3);
         printf("Jogo terminado. Voce acertou os 3 navios em %d tentativas.\n\n", tentativas);
-        mostraTabuleiro();
+        mostrar_tabuleiro();
         do {
             puts("Deseja jogar de novo:");
             puts("0 para nao.");
@@ -49,7 +49,7 @@ int main(void) {
     return 0;
 }
 
-void inicializaTabuleiro() {
+void inicializar_tabuleiro() {
     int l, c;
     for (l = 0; l < 5; l++) {
         for (c = 0; c < 5; c++) {
@@ -58,25 +58,25 @@ void inicializaTabuleiro() {
     }
 }
 
-void mostraTabuleiro() {
+void mostrar_tabuleiro() {
     puts("    1    2    3    4    5");
-    int l, c;
-    for (l = 0; l < 5; l++) {
-        printf("%d   ", (l + 1));
-        for (c = 0; c < 5; c++) {
-            if (c < 4)
-                printf("%c    ", tabuleiro[l][c]);
+    int linha, coluna;
+    for (linha = 0; linha < 5; linha++) {
+        printf("%d   ", (linha + 1));
+        for (coluna = 0; coluna < 5; coluna++) {
+            if (coluna < 4)
+                printf("%c    ", tabuleiro[linha][coluna]);
             else
-                printf("%c", tabuleiro[l][c]);
+                printf("%c", tabuleiro[linha][coluna]);
         }
         puts("");
-        c = 0;
+        coluna = 0;
     }
     puts("");
 }
 
-void iniciaNavios() {
-    int i, ii, linha, coluna, continuar;
+void iniciar_navios() {
+    int i, j, linha, coluna, continuar;
 
     for (i = 0; i < 3; i++) {
         do {
@@ -86,14 +86,14 @@ void iniciaNavios() {
             coluna = (rand() % 5) + 1;
             navio[i][0] = linha;
             navio[i][1] = coluna;
-            for (ii = 0; ii < i; ii++) {
-                if (navio[i][0] == navio[ii][0] && navio[ii][1] == navio[i][1]) continuar = 1;
+            for (j = 0; j < i; j++) {
+                if (navio[i][0] == navio[j][0] && navio[j][1] == navio[i][1]) continuar = 1;
             }
         } while (continuar);
     }
 }
 
-void darTiro() {
+void atirar() {
     int linha, coluna;
     do {
         do {
@@ -113,18 +113,18 @@ void darTiro() {
         } while (coluna < 1 || coluna > 5);
         tiro[0] = linha;
         tiro[1] = coluna;
-    } while (verificaPosicao());
+    } while (verificar_posicao());
 
     tentativas++;
 
     if (acertou(linha, coluna)) {
         dica();
         printf("Voce acertou o tiro (%d,%d)\n\n", tiro[0], tiro[1]);
-        alteraTabuleiro('X');
+        alterar_tabuleiro('X');
 
     } else {
         dica();
-        alteraTabuleiro('*');
+        alterar_tabuleiro('*');
     }
 }
 
@@ -139,17 +139,17 @@ int acertou(int linha, int coluna) {
     return 0;
 }
 
-void alteraTabuleiro(char pos) {
+void alterar_tabuleiro(char pos) {
     tabuleiro[tiro[0] - 1][tiro[1] - 1] = pos;
 }
 
 void dica() {
-    int l, c, nl = 0, nc = 0;
-    for (l = 0; l < 3; l++) {
-        if (tiro[0] == navio[l][0]) nl++;
+    int linha, coluna, nl = 0, nc = 0;
+    for (linha = 0; linha < 3; linha++) {
+        if (tiro[0] == navio[linha][0]) nl++;
     }
-    for (c = 0; c < 3; c++) {
-        if (tiro[1] == navio[c][1]) nc++;
+    for (coluna = 0; coluna < 3; coluna++) {
+        if (tiro[1] == navio[coluna][1]) nc++;
     }
 
     puts("Dica:");
@@ -157,13 +157,12 @@ void dica() {
     printf("Coluna %d -> %d navio(s)\n\n", tiro[1], nc);
 }
 
-int verificaPosicao() {
+int verificar_posicao() {
     if (tabuleiro[tiro[0] - 1][tiro[1] - 1] == '~') return 0;
     puts("Posicao ocupada, tente de novo.\n");
     return 1;
 }
 
-void limparTerminal() {
-    int i;
-    for (i = 1; i <= 100; i++) puts("");
+void limpar_terminal() {
+    for (int i = 0; i < 100; i++) printf("\n");
 }
