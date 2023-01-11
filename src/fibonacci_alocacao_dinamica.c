3b94fdda2adbf5c205cf4c7f52358b64e6ad2472
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-01-11 15:19:54 -0300

    fibonacci_alocacao_dinamica

--- src/fibonacci_alocacao_dinamica.c
+++ src/fibonacci_alocacao_dinamica.c
@@ -1,64 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int menu(void)
-{
-    int opcao;
-    printf("Escolha uma opcao.\n"
-           "0. Sair.\n"
-           "1. Escolher um termo.\n"
-           "Opcao: ");
-    scanf("%d", &opcao);
-    printf("\n");
-    return opcao;
-}
-
-void exibe(int *ptr, int termo, int *size)
-{
-    if (termo > *size)
-    {
-        int v = (*size*sizeof(int));
-        int n = ((termo-*size)*sizeof(int));
+void exibe(int *ptr, int termo, int *size) {
+    if (termo > *size) {
+        int v = (*size * sizeof(int));
+        int n = ((termo - *size) * sizeof(int));
         printf("Velho %d\nNovo %d\n\n", v, n);
-        ptr = (int *) realloc(ptr, v+n);
+        ptr = realloc(ptr, v + n);
         int i;
-        for(i = *size; i < termo; i++)
-        {
-            ptr[*size] = ptr[*size-1] + ptr[*size-2];
+        for (i = *size; i < termo; i++) {
+            ptr[*size] = ptr[*size - 1] + ptr[*size - 2];
             *size += 1;
         }
-        printf("Termo %d = %d\n\n", termo, ptr[termo-1]);
-    }
-    else
-    {
-        printf("Termo %d = %d\n\n", termo, ptr[termo-1]);
+        printf("Termo %d = %d\n\n", termo, ptr[termo - 1]);
+    } else {
+        printf("Termo %d = %d\n\n", termo, ptr[termo - 1]);
     }
 }
 
-int main(void)
-{
-    int opcao = 1, t, s = 2;
-    int *p;
-    p = (int *) malloc(2*sizeof(int));
-    p[0] = 0;
-    p[1] = 1;
-    do
-    {
-        switch(menu())
-        {
-        case 0:
-            opcao = 0;
-            break;
-        case 1:
-            printf("Digite um termo: ");
-            scanf("%d", &t);
-            printf("\n");
-            exibe(p, t, &s);
-            break;
-        default:
-            printf("Opcao invalida.\n\n");
+int calcular_fibonacci(int *sequencia_fibonacci, int *termos_totais, int termo) {
+    if (termo > *termos_totais) {
+        int novo_tamanho_memoria = termo * sizeof(int);
+        sequencia_fibonacci = realloc(sequencia_fibonacci, novo_tamanho_memoria);
+        
+        for (int i = *termos_totais; i < termo; i++) {
+            sequencia_fibonacci[i] = sequencia_fibonacci[i - 1] + sequencia_fibonacci[i - 2];
         }
+        *termos_totais = termo;
     }
-    while(opcao);
+    return sequencia_fibonacci[termo - 1];
+}
+
+int main(void) {
+    int posicao = 0, termos_totais = 2;
+    int *sequencia_fibonacci = malloc(termos_totais * sizeof(int));
+    sequencia_fibonacci[0] = 0;
+    sequencia_fibonacci[1] = 1;
+
+    puts("Sequência de Fibonacci");
+    puts("Digite a posição do termo que você quer ver ou 0 para encerrar.\n");
+
+    do {
+        printf("Digite a posição do termo: ");
+        scanf("%d", &posicao);
+        if (posicao <= 0) break;
+
+        int valor = calcular_fibonacci(sequencia_fibonacci, &termos_totais, posicao);
+        printf("O %dº termo na sequência de Fibonacci é %d\n\n", posicao, valor);
+        printf("*** %d\n\n", termos_totais);
+    } while (posicao > 0);
+
+    puts("\nPrograma encerrado.");
     return 0;
 }
