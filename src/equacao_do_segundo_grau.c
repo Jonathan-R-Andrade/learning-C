aede9c9866ba19047a544abbde7b5fb2d5e52d11
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-01-12 19:42:11 -0300

    equacao_do_segundo_grau

--- src/equacao_do_segundo_grau.c
+++ src/equacao_do_segundo_grau.c
@@ -15,18 +15,20 @@ int calcular_raiz_2(int delta, int a, int b) {
 
 void calcular_equacao_2_grau(int a, int b, int c) {
     if (a != 0) {
-        int d = calcular_delta(a, b, c);
-        if (d < 0) {
-            printf("Essa equação não possui raiz real.");
+        int delta = calcular_delta(a, b, c);
+        if (delta < 0) {
+            printf("Essa equação não possui raiz real.\n");
         } else {
-            printf("Raiz 1 = %d", calcular_raiz_1(d, a, b));
-            printf("\nRaiz 2 = %d", calcular_raiz_2(d, a, b));
+            printf("Raiz 1 = %d\n", calcular_raiz_1(delta, a, b));
+            printf("Raiz 2 = %d\n", calcular_raiz_2(delta, a, b));
         }
     } else {
-        printf("Essa equação não é válida.");
+        printf("Essa equação não é válida.\n");
     }
 }
 
 int main() {
     calcular_equacao_2_grau(0, 8, 16);
+    calcular_equacao_2_grau(5, 2, 20);
+    calcular_equacao_2_grau(3, 15, 66);
 }
