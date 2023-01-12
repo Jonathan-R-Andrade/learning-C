4a99b23cae94d797ae312e53a8d947c17f8445f1
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-01-12 19:38:36 -0300

    equacao_do_segundo_grau

--- src/equacao_do_segundo_grau.c
+++ src/equacao_do_segundo_grau.c
@@ -1,26 +1,26 @@
 #include <math.h>
 #include <stdio.h>
 
-int delta(int a, int b, int c) {
+int calcular_delta(int a, int b, int c) {
     return pow(b, 2) - (4 * a * c);
 }
 
-int raiz1(int delta, int a, int b) {
+int calcular_raiz_1(int delta, int a, int b) {
     return (-b + sqrt(delta)) / (2 * a);
 }
 
-int raiz2(int delta, int a, int b) {
+int calcular_raiz_2(int delta, int a, int b) {
     return (-b - sqrt(delta)) / (2 * a);
 }
 
-void equacao2grau(int a, int b, int c) {
+void calcular_equacao_2_grau(int a, int b, int c) {
     if (a != 0) {
-        int d = delta(a, b, c);
+        int d = calcular_delta(a, b, c);
         if (d < 0) {
             printf("Essa equação não possui raiz real.");
         } else {
-            printf("Raiz 1 = %d", raiz1(d, a, b));
-            printf("\nRaiz 2 = %d", raiz2(d, a, b));
+            printf("Raiz 1 = %d", calcular_raiz_1(d, a, b));
+            printf("\nRaiz 2 = %d", calcular_raiz_2(d, a, b));
         }
     } else {
         printf("Essa equação não é válida.");
@@ -28,5 +28,5 @@ void equacao2grau(int a, int b, int c) {
 }
 
 int main() {
-    equacao2grau(0, 8, 16);
+    calcular_equacao_2_grau(0, 8, 16);
 }
